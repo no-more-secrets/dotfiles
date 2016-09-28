c21fb2cf7a94f75eb9b2810776252338e28bd9a9
David P. Sicilia <dpsicilia@gmail.com>
2016-09-28 17:47:20 -0400

    some updates

--- make_links.sh
+++ make_links.sh
@@ -5,8 +5,6 @@ rm $HOME/.ghc/ghci.conf
 ln -s $(pwd)/dot-ghc-slash-ghci.conf $HOME/.ghc/ghci.conf
 rm $HOME/.gitconfig
 ln -s $(pwd)/dot-gitconfig $HOME/.gitconfig
-rm $HOME/.ssh/config
-ln -s $(pwd)/dot-ssh-slash-config $HOME/.ssh/config
 rm -rf $HOME/.vim
 ln -s $(pwd)/dot-vim-folder $HOME/.vim
 rm $HOME/.vimrc
