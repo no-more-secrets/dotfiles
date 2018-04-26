065a89c1f20834e8516cf4227243706d3681e396
David P. Sicilia <unspecified>
2018-04-26 13:49:26 -0400

    changes

--- dot/config/fish/config.fish
+++ dot/config/fish/config.fish
@@ -1,2 +1,11 @@
+function my_vi_bindings
+    fish_vi_key_bindings
+    # This will allow us to retain the default fish C-F (accept suggestion)
+    # feature when VI mode is enabled.
+    bind -M insert  \cf forward-char
+    bind -M default \cf forward-char
+    bind -M visual  \cf forward-char
+end
 
-set -g fish_key_bindings fish_vi_key_bindings
+set -g fish_key_bindings my_vi_bindings
+#set -g fish_key_bindings fish_vi_key_bindings
