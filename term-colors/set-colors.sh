769834b4e24f7123dd89141ec1b36f1240d30fb9
David P. Sicilia <unspecified>
2018-08-03 18:45:52 -0400

    changes

---
+++ term-colors/set-colors.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#!/bin/bash
+set -e
+
+cd $(dirname $(readlink -f $0))
+
+profiles="/org/gnome/terminal/legacy/profiles:/"
+
+profile_id=$(dconf list $profiles)
+echo "List of profiles:"
+echo "$profile_id"
+
+[[ ! -z "$profile_id"         ]]
+[[ "$profile_id" =~ :[^:]{36} ]]
+
+full_profile="$profiles$profile_id"
+
+echo "Full profile:"
+echo "$full_profile"
+
+settings=$(dconf list $full_profile)
+echo "List of settings:"
+echo "$settings"
+
+[[ "$settings" =~ foreground-color ]]
+[[ "$settings" =~ background-color ]]
+
+foreground="'$(<foreground.txt)'"
+background="'$(<background-matte.txt)'"
+
+dconf write ${full_profile}background-color "$background"
+dconf write ${full_profile}foreground-color "$foreground"
+
+echo Success.
