fb293788556d8c36f50e5c2c785f5486c985d9b4
David P. Sicilia <unspecified>
2018-08-06 17:30:09 -0400

    changes

--- dot/config/fish/functions/binding-C-p.fish
+++ dot/config/fish/functions/binding-C-p.fish
@@ -1,18 +1,22 @@
 function binding-C-p
     set -l result ""
+    set -l handled 1 # initially not handled
 
-    if git rev-parse --is-inside-work-tree 2>/dev/null
+    if [ ! $handled -eq 0 ]; and git rev-parse --is-inside-work-tree 1>/dev/null 2>/dev/null
         # Simply-parsable list of relative paths (relative to CWD), ignoring
         # with no renames + ignoring deleted files (so therefore if a file is
         # renamed the new name will appear in the list).
-        set result (git status --short --no-renames | grep -v '^D' | awk '{print $2}' | fzf)
-    else if functions -q local-binding-C-p
-        set result (local-binding-C-p)
+        set result (git status --short --no-renames | grep -v '^D' | awk '{print $2}' | fzf); set handled $status
     end
 
-    # If the commandline is empty then put a `vim` in front of it because
-    # we will likely want to edit the result.
-    if string length -q $result; and not string length -q (commandline)
+    if [ ! $handled -eq 0 ]; and functions -q local-binding-C-p
+        set result (local-binding-C-p); set handled $status
+    end
+
+    # If the commandline is empty and the file is not executable then
+    # then put a `vim` in front of it because# we will likely want to
+    # edit the result.
+    if string length -q $result; and not string length -q (commandline); and [ ! -x $result ]
         set result vim $result
     end
 
--- dot/config/fish/functions/binding-C-t.fish
+++ dot/config/fish/functions/binding-C-t.fish
@@ -1,21 +1,26 @@
 function binding-C-t
     set -l result ""
+    set -l handled 1 # initially not handled
 
-    if string match -q -r "^[ ]*kill " (commandline)
-        set result (fzf-kill)
-        repaint-cmd-line $result
-        return
+    if [ ! $handled -eq 0 ]; and string match -q -r "^[ ]*kill " (commandline)
+        set result (fzf-kill); set handled $status
     end
 
-    if functions -q local-binding-C-t
-        set result (local-binding-C-t); and begin
-            repaint-cmd-line $result
-            return
-        end
+    if [ ! $handled -eq 0 ]; and functions -q local-binding-C-t
+        set result (local-binding-C-t); set handled $status
     end
 
     # Last resort
-    set result (fzf)
+    if [ ! $handled -eq 0 ]
+        set result (fzf); set handled $status
+    end
+
+    # If the commandline is empty and the file is not executable then
+    # then put a `vim` in front of it because# we will likely want to
+    # edit the result.
+    if string length -q $result; and not string length -q (commandline); and [ ! -x $result ]
+        set result vim $result
+    end
 
     repaint-cmd-line $result
 end
