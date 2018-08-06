489a10c741684757a54997165676191f47f20c87
David P. Sicilia <unspecified>
2018-08-06 18:59:45 -0400

    changes

--- dot/config/fish/functions/binding-C-e.fish
+++ dot/config/fish/functions/binding-C-e.fish
@@ -1,5 +1,14 @@
 function binding-C-e
-    set -l result (eval $fzf_ctrl_e_cmd 2>/dev/null | fzf --no-preview --no-multi)
+    set -l result ""
+    set -l handled 1 # initially not handled
+
+    if [ ! $handled -eq 0 ]; and functions -q local-binding-C-e
+        set result (local-binding-C-e); set handled $status
+    end
+
+    if [ ! $handled -eq 0 ]
+        set result (eval $fzf_ctrl_e_cmd 2>/dev/null | fzf --no-preview --no-multi)
+    end
 
     string length -q $result; and cd $result
 
