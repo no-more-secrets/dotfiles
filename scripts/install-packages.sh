75adfffd27564ff07f694d54bbb6a11c5bf3d133
David P. Sicilia <unspecified>
2018-08-20 18:11:38 -0400

    changes

---
+++ scripts/install-packages.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+list="
+    ack
+    autoconf
+    automake
+    bison
+    build-essential
+    cmake
+    curl
+    exuberant-ctags
+    fd
+    fish
+    flex
+    fonts-liberation
+    git
+    haskell-stack
+    htop
+    libncurses5
+    libncurses5-dev
+    libncursesw5
+    libncursesw5-dev
+    libsqlite3-0
+    libsqlite3-dev
+    libxml2
+    libxml2-dev
+    libzip4
+    libzip-dev
+    perl
+    python2.7
+    python2.7-dev
+    python3
+    python3-dev
+    silversearcher-ag
+    tmux
+    vim
+    wget
+    xxd
+    zip
+"
+
+sudo apt install $list
