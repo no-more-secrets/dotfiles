54f77331e6884f3ba6ed588c7a0813e3df45169a
David P. Sicilia <dpsicilia@gmail.com>
2020-05-13 11:57:08 -0400

    changes

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -29,17 +29,17 @@ list="
     libncurses5-dev
     libncursesw5
     libncursesw5-dev
+    libsdl2-$sdl_version
     libsdl2-dev
     libsdl2-doc
-    libsdl2-image-dev
     libsdl2-image-$sdl_version
-    libsdl2-mixer-dev
+    libsdl2-image-dev
     libsdl2-mixer-$sdl_version
-    libsdl2-net-dev
+    libsdl2-mixer-dev
     libsdl2-net-$sdl_version
-    libsdl2-$sdl_version
-    libsdl2-ttf-dev
+    libsdl2-net-dev
     libsdl2-ttf-$sdl_version
+    libsdl2-ttf-dev
     libsqlite3-0
     libsqlite3-dev
     libssl-dev
@@ -62,6 +62,8 @@ list="
     python2.7-dev
     python3
     python3-dev
+    python3-pip
+    python-pip
     qt5-default
     screenfetch
     silversearcher-ag
