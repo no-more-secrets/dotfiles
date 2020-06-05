17fce9de36b784f43d9e17256687cf496fe12cbc
David P. Sicilia <dpsicilia@gmail.com>
2020-06-05 16:36:36 -0400

    changes

--- dot/config/fish/functions/fish_default_mode_prompt.fish
+++ dot/config/fish/functions/fish_default_mode_prompt.fish
@@ -1,9 +1,8 @@
 # We override the default version (usually located in
-# /usr/share/fish/functions/fish_default_mode_prompt.fish
-# so that when in insert mode (which behaves like a normal
-# shell prompt) there will be no indicator.  Then, only
-# when switching to normal mode, replace mode, or visual
-# mode will there be an indicator.
+# /usr/share/fish/functions/fish_default_mode_prompt.fish so that
+# when in insert mode (which behaves like a normal shell prompt)
+# there will be no indicator. Then, only when switching to normal
+# mode, replace mode, or visual mode will there be an indicator.
 
 function fish_default_mode_prompt --description "Display the default mode for the prompt"
     # Do nothing if not in vi mode
@@ -11,22 +10,23 @@ function fish_default_mode_prompt --description "Display the default mode for th
         or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
         switch $fish_bind_mode
             case default
-                set_color --bold --background yellow white
-                echo '[N]'
+                set_color --bold bryellow
+                echo 'n'
 				set_color normal
 				echo -n ' '
             case insert
-                set_color --bold --background green white
-                #echo '[I]'
+                set_color --dim grey
+                echo 'i'
 				set_color normal
+				echo -n ' '
             case replace_one
-                set_color --bold --background cyan white
-                echo '[R]'
+                set_color --bold brcyan
+                echo 'r'
 				set_color normal
 				echo -n ' '
             case visual
-                set_color --bold --background magenta white
-                echo '[V]'
+                set_color --bold brmagenta
+                echo 'v'
 				set_color normal
 				echo -n ' '
         end
