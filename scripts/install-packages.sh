4423bab226dea27057f9bcfc09caed622cd5572f
David P. Sicilia <dpsicilia@gmail.com>
2020-06-05 18:18:09 -0400

    changes

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -26,21 +26,22 @@ list="
     libdw-dev
     libfreetype6
     libfreetype6-dev
+    libharfbuzz-dev
     libncurses5
     libncurses5-dev
     libncursesw5
     libncursesw5-dev
+    libsdl2-$sdl_version
     libsdl2-dev
     libsdl2-doc
-    libsdl2-image-dev
     libsdl2-image-$sdl_version
-    libsdl2-mixer-dev
+    libsdl2-image-dev
     libsdl2-mixer-$sdl_version
-    libsdl2-net-dev
+    libsdl2-mixer-dev
     libsdl2-net-$sdl_version
-    libsdl2-$sdl_version
-    libsdl2-ttf-dev
+    libsdl2-net-dev
     libsdl2-ttf-$sdl_version
+    libsdl2-ttf-dev
     libsqlite3-0
     libsqlite3-dev
     libssl-dev
