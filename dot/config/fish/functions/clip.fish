9f8b99159fa43db4e11b9caabecfe02ad8f404ec
David P. Sicilia <dpsicilia@gmail.com>
2020-06-07 07:29:57 -0400

    changes

--- dot/config/fish/functions/clip.fish
+++ dot/config/fish/functions/clip.fish
@@ -1,4 +1,8 @@
 # Clips output to the number of columns on the display
 function clip
+  if test -x ~/dev/utilities/clip/.builds/current/exe
+    ~/dev/utilities/clip/.builds/current/exe $COLUMNS
+  else
     sed -ru 's/(.{0,'$COLUMNS'}).*/\1/g'
+  end
 end
