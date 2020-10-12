9362cf7daafaf0ec7d631df9f0fc6162cafb0b54
David P. Sicilia <dpsicilia@gmail.com>
2020-10-12 00:25:22 -0400

    changes

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -24,9 +24,12 @@ list="
     htop
     libcurl4-openssl-dev
     libdw-dev
+    libfontconfig1-dev
     libfreetype6
     libfreetype6-dev
+    libgl1-mesa-dev
     libharfbuzz-dev
+    liblua5.3-dev
     libncurses5
     libncurses5-dev
     libncursesw5
@@ -46,15 +49,19 @@ list="
     libsqlite3-dev
     libssl-dev
     libtool
-    libtool
     libtool-bin
     libwebp-dev
+    libx11-dev
+    libxcb-render0
+    libxcb-render0-dev
+    libxcb-shape0
+    libxcb-shape0-dev
+    libxcb-xfixes0
+    libxcb-xfixes0-dev
+    libxcursor-dev
     libxml2
     libxml2-dev
-    libzip4
-    libzip-dev
     lua5.3
-    lua5.3-dev
     ninja-build
     nmap
     openssh-server
@@ -62,12 +69,13 @@ list="
     perl
     powertop
     protobuf-compiler
+    python
     python2.7
     python2.7-dev
     python3
     python3-dev
     python3-pip
-    python-pip
+    python-pip-whl
     qt5-default
     screenfetch
     silversearcher-ag
@@ -77,11 +85,17 @@ list="
     tree
     vim
     wget
+    xclip
     zip
 "
+
+sudo apt-add-repository -y ppa:system76-dev/stable
+sudo apt update
 
 sudo apt install $list
 
 bash scripts/install-fd.sh
 
 bash scripts/install-stack.sh
+
+sudo apt install system76-power
