d648cc63976f945cd11e8fbb29812049a9988f33
David P. Sicilia <dpsicilia@gmail.com>
2021-01-05 16:26:13 -0500

    changes

--- dot/config/fish/functions/ag.fish
+++ dot/config/fish/functions/ag.fish
@@ -1,3 +1,3 @@
 function ag
-    env ag --one-device --color-path='2;15' --color-match='4;230' --color-line-number='1;33' $argv
+    env ag --one-device --color-path='2;15' --color-match='1;36' --color-line-number='1;33' $argv
 end
