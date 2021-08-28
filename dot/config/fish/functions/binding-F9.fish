a969643292bd00084245f89e3558a1291df9beaa
David P. Sicilia <dpsicilia@gmail.com>
2021-08-28 09:21:22 -0400

    changes

--- dot/config/fish/functions/binding-F9.fish
+++ dot/config/fish/functions/binding-F9.fish
@@ -1,15 +1,7 @@
-# Runs `make test` with appropriate number of cores.  If
-# shift is held then it will be OPT=.
+# Runs `make all` without stopping on errors.
 function binding-F9
   test -e Makefile; or return 1
-  set -l cores (cores)
-  set -l cmd "make test -j$cores"
-  if test (count $argv) -gt 0
-    set -l modifier $argv[1]
-    if string match -q $modifier "shift"
-      set cmd "$cmd OPT="
-    end
-  end
+  set -l cmd "make all KEEP_GOING=1"
   echo $cmd
   eval $cmd
   # This is a hack to allow the error code to propagate out of
