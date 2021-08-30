9709ca233e84baff6d0af00a89bf8f913499d24c
David P. Sicilia <dpsicilia@gmail.com>
2021-08-30 11:12:24 -0400

    changes

--- dot/config/fish/functions/binding-C-y.fish
+++ dot/config/fish/functions/binding-C-y.fish
@@ -1,3 +1,14 @@
+# Runs `make test` with appropriate number of cores.  If
+# shift is held then it will be OPT=.
 function binding-C-y
-  binding-F9
+  test -e Makefile; or return 1
+  set -l cores (cores)
+  set -l cmd "make test -j$cores"
+  echo $cmd
+  eval $cmd
+  # This is a hack to allow the error code to propagate out of
+  # this function and into the visual indicator in the command
+  # prompt.
+  set -g status_cache $status
+  commandline -f repaint
 end
