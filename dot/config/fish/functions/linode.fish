ead7a4462253f3c14a45e68c130623529d153ebd
David P. Sicilia <dpsicilia@gmail.com>
2021-11-14 10:43:25 -0500

    changes

--- dot/config/fish/functions/linode.fish
+++ dot/config/fish/functions/linode.fish
@@ -2,5 +2,14 @@
 # when subsequently detaching from the session, it will also au-
 # tomatically log out as well.
 function linode
+  # The -t option is a special ssh option that appears to be re-
+  # quired for this exact purpose.  From the ssh man page:
+  #
+  #   -t Force pseudo-terminal allocation. This can be used to
+  #      execute arbitrary screen-based programs on a remote ma-
+  #      chine, which can be very useful, e.g. when implementing
+  #      menu services. Multiple -t options force tty allocation,
+  #      even if ssh has no local tty.
+  #
   ssh linode -t tmux attach
 end
