07648d1279a10e318b4b1c200adabae7fde18134
David P. Sicilia <dpsicilia@gmail.com>
2022-01-20 12:49:08 -0500

    changes

--- dot/config/fish/functions/branch.fish
+++ dot/config/fish/functions/branch.fish
@@ -1,7 +1,7 @@
 # Displays the current branch name (only -- no asterisk) of the
 # local repo.
 function branch
-    set -l name (git rev-parse --abbrev-ref HEAD ^/dev/null)
+    set -l name (git rev-parse --abbrev-ref HEAD 2>/dev/null)
     and echo $name
     # !! Will propagate error code if git command failed.
 end
