74e6cf28eb986e514b8d797d0b81c8866a9a0a19
David P. Sicilia <dpsicilia@gmail.com>
2023-02-28 12:03:39 -0500

    changes

--- dot/config/nvim/lua/dsicilia/init.lua
+++ dot/config/nvim/lua/dsicilia/init.lua
@@ -1,5 +1,6 @@
-require 'dsicilia.options'
-require 'dsicilia.keymap'
+require'dsicilia.options'
+require'dsicilia.keymap'
+require'dsicilia.rn'
 
 -- Temporary until migration is complete.
 vim.cmd[[source ~/.vimrc]]
---
+++ dot/config/nvim/lua/dsicilia/rn.lua
@@ -0,0 +1,18 @@
+-- This module ensures that when we are in the rn folder struc-
+-- ture that we autoload any .vimrc files that we find there,
+-- which are trusted. Specifically, any time we open neovim in a
+-- subfolder of the "revolution-now" folder, it will load the
+-- .vimrc from the root folder.
+--
+-- Background: Compared with Vim, Neovim uses the safer default
+-- of not auto-loading a .vimrc in the current folder. This is
+-- good as a general rule and we want to keep it. However, there
+-- is an exception which is fairly safe.
+local format = string.format
+
+local cwd = vim.fn.getcwd()
+local rn_start, rn_end = cwd:find( '.*revolution%-now' )
+if rn_start then
+  local rn = cwd:sub( rn_start, rn_finish )
+  vim.cmd( format( 'source %s/.vimrc', rn ) )
+end
