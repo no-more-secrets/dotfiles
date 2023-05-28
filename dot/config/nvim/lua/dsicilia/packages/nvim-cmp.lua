a3308ed0950768cd6d5dd5014f47fdf88ba552ed
David P. Sicilia <dpsicilia@gmail.com>
2023-05-28 11:45:42 -0400

    changes

--- dot/config/nvim/lua/dsicilia/packages/nvim-cmp.lua
+++ dot/config/nvim/lua/dsicilia/packages/nvim-cmp.lua
@@ -27,8 +27,8 @@ cmp.setup( {
   },
 
   window = {
-    --completion = cmp.config.window.bordered(),
-    --documentation = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
 
   experimental = {
