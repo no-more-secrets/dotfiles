d02a929c684c7f59d0ef73fc12be1b562a3c3df2
David P. Sicilia <dpsicilia@gmail.com>
2023-05-28 19:30:14 -0400

    changes

--- dot/config/nvim/lua/dsicilia/packages/leap.lua
+++ dot/config/nvim/lua/dsicilia/packages/leap.lua
@@ -18,3 +18,12 @@ local nmap = mappers.nmap
 -----------------------------------------------------------------
 nmap['s'] = '<Plug>(leap-forward-to)'
 nmap['S'] = '<Plug>(leap-backward-to)'
+
+-- Bidirectional. Uncomment this is the directional keys above
+-- don't work out; this has a disadvantage that it won't auto
+-- jump to the closest match, like the directional versions will,
+-- since it doesn't know which direction you want to go.
+-- nmap[';'] = function ()
+--   local current_window = vim.fn.win_getid()
+--   require( 'leap' ).leap{ target_windows={ current_window } }
+-- end
