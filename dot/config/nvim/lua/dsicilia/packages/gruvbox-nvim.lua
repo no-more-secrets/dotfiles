f407dc9e69902c9c3fbc8d3698f3a3f784a473bf
David P. Sicilia <dpsicilia@gmail.com>
2023-06-02 07:53:04 -0400

    changes

--- dot/config/nvim/lua/dsicilia/packages/gruvbox-nvim.lua
+++ dot/config/nvim/lua/dsicilia/packages/gruvbox-nvim.lua
@@ -16,24 +16,12 @@ local INVERT = config.invert_signs
 
 -- Setup must be called before loading the colorscheme.
 gruvbox.setup( {
-  undercurl=true,
-  underline=true,
-  bold=true,
   italic={
     strings=false,
     comments=true,
     operators=false,
     folds=true,
   },
-  strikethrough=true,
-  invert_selection=false,
-  invert_signs=false,
-  invert_tabline=false,
-  invert_intend_guides=false,
-  inverse=true, -- invert background for search, diffs, statuslines and errors
-  contrast='', -- can be "hard", "soft" or empty string
-  dim_inactive=false,
-  transparent_mode=false,
   palette_overrides={
     -- This is so that the background of vim matches the back-
     -- ground of our terminal.
