fa791a8ebb785158ceb80511edf1428063183919
David P. Sicilia <dpsicilia@gmail.com>
2023-06-04 10:24:29 -0400

    changes

--- dot/config/nvim/lua/dsicilia/init.lua
+++ dot/config/nvim/lua/dsicilia/init.lua
@@ -9,6 +9,7 @@ require'dsicilia.rn'
 require'dsicilia.status-bar'
 require'dsicilia.colors'
 require'dsicilia.format'
+require'dsicilia.dev'
 
 -- Temporary until migration is complete.
 vim.cmd[[source ~/.vimrc]]
--- dot/config/nvim/lua/dsicilia/packages/telescope.lua
+++ dot/config/nvim/lua/dsicilia/packages/telescope.lua
@@ -32,6 +32,15 @@ telescope.setup{
     -- window.
     sorting_strategy='ascending',
 
+    extensions={
+      fzf={
+        fuzzy=true, -- false will only do exact matching
+        override_generic_sorter=true, -- override the generic sorter
+        override_file_sorter=true, -- override the file sorter
+        case_mode='smart_case',
+      },
+    },
+
     mappings={
       i={
         -- Shows the mappings for your picker.
