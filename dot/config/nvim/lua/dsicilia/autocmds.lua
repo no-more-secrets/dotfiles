51f79d2138f3bff897991421fb5f0a3b4d9dd6fb
David P. Sicilia <dpsicilia@gmail.com>
2023-06-20 22:14:25 -0400

    changes

--- dot/config/nvim/lua/dsicilia/autocmds.lua
+++ dot/config/nvim/lua/dsicilia/autocmds.lua
@@ -18,17 +18,16 @@ local line = vim.fn.line
 -----------------------------------------------------------------
 -- Return to last cursor pos.
 -----------------------------------------------------------------
--- Return to last edit position when opening files and make that
--- line in the center of the screen.
+-- Return to last edit position when entering a window (either
+-- when first opening neovim, or thereafter) and make that line
+-- in the center of the screen.
 local function restore_cursor()
-  vim.schedule( function()
-    local last_pos = line[['"]]
-    if last_pos == 0 or last_pos > line( '$' ) then return end
-    vim.cmd[[normal! g`"z.]]
-  end )
+  local last_pos = line[['"]]
+  if last_pos == 0 or last_pos > line( '$' ) then return end
+  vim.cmd[[normal! g`"zz]]
 end
 
-autocmd( 'BufReadPost', {
+autocmd( 'BufWinEnter', {
   group=augroup( 'RestoreCursor', { clear=true } ),
   callback=restore_cursor,
 } )
