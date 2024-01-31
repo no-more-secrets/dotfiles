0a5179bb93be81342f0a4fa3d4677d1bc03547fe
David P. Sicilia <dpsicilia@gmail.com>
2024-01-30 20:59:50 -0500

    changes

--- dot/config/nvim/lua/dsicilia/packages/nvim-treesitter.lua
+++ dot/config/nvim/lua/dsicilia/packages/nvim-treesitter.lua
@@ -54,7 +54,7 @@ require( 'nvim-treesitter.configs' ).setup{
     -- disable = DISABLE,
     disable=function( lang, buf )
       if DISABLE[lang] then return true end
-      local max_filesize = 1024 * 1024
+      local max_filesize = 100 * 1024 -- 100 KB
       local ok, stats = pcall( vim.loop.fs_stat,
                                nvim_buf_get_name( buf ) )
       if ok and stats and stats.size > max_filesize then
